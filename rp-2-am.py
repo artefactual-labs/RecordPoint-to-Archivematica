d9ace3cc748e045273ad180c97df3a923e4008b4
Peter Van Garderen <peter@artefactual.com>
2021-03-03 17:26:44 -0800

    WIP convert RP metadata to AM metadata.csv

--- rp-2-am.py
+++ rp-2-am.py
@@ -31,16 +31,41 @@ def parse_rp_exports(export_directory):
         return
 
     try:
-        with open(export_dir + rp_csv, mode='r') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            line_count = 0
-            for row in csv_reader:
-                if line_count == 0:
-                    print(f'Column names are {", ".join(row)}')
-                    line_count += 1
-                print(f'\t{row["Title"]} has the identifier {row["ItemNumber"]} and was kept in the {row["LibraryName"]} library.')
-                line_count += 1
-            print(f'Processed {line_count} lines.')
+        with open(export_dir + rp_csv, mode='r') as rp_csv_file:
+            csv_reader = csv.DictReader(rp_csv_file)
+            file_count = 0
+
+            if not os.path.exists('transfers'):
+                os.makedirs('transfers')
+
+            '''
+            TODO: slice out RP export directory name from export_dir to use
+            as Archivematica transfer directory name: 'transfers/2021-03-02_10-24/metadata.csv'
+            '''
+
+            with open('metadata.csv', mode='w') as archivematica_csv_file:
+                csv_writer = csv.writer(archivematica_csv_file, delimiter=',')
+                csv_writer.writerow(['dc.title', 'dc.identifier', 'dc.identifier', 'dc.creator', 'dc.contributor', 'dc.date', 'dc.format', 'dc.description', 'record category', 'vital record', 'location', 'content version', 'content type name', 'created date', 'file size', 'file type', 'last modified', 'library name', 'site name', 'streamhash', 'document type'])
+
+                #TODO: seperate try/except blocks for read & write
+
+                for row in csv_reader:
+                    # files with 'Destroy' retention are listed in the CSV file
+                    # but are not included in the export directory.
+                    if row["Title"] in files:
+
+                        #TODO: write row to metadata.csv
+
+                        print(f'\t{row["Title"]} has the identifier {row["ItemNumber"]} and was kept in the {row["LibraryName"]} library.')
+                        #TODO: add file move to AM transfer directory
+
+                        #TODO: try to sort files by SharePoint library
+                        file_count += 1
+                print(f'Added {file_count} files to ...') #TODO add tranfer directory name
+
+                # TODO add `-d` flag to command and function parameters. Delete
+                # the RP transfer directory if set.
+
     except Exception as e:
         print("Error: cannot parse the SharePoint-Disposition-Trigger CSV file: " + str(e))
 
